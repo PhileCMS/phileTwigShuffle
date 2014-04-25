cbacd95d05bbacf54697b94caa600572948582b6
Frank Nägler <mail@naegler.net>
2014-04-25 19:51:58 +0200

    update to 1.0.0

--- Classes/Plugin.php
+++ Classes/Plugin.php
@@ -1,19 +1,26 @@
 <?php
+/**
+ * Plugin class
+ */
+namespace Phile\Plugin\Phile\TwigShuffle;
 
 /**
  * Shuffle an associative array using Twig
  * Usage: {{ shuffle(array) }}
  */
-
-class PhileTwigShuffle extends \Phile\Plugin\AbstractPlugin implements \Phile\EventObserverInterface {
+class Plugin extends \Phile\Plugin\AbstractPlugin implements \Phile\Gateway\EventObserverInterface {
+	/**
+	 * the constructor
+	 */
 	public function __construct() {
 		\Phile\Event::registerEvent('template_engine_registered', $this);
 	}
 
-	/*!
+	/**
 	 * shuffle an associative array
-	 * @param  array $array the input array
-	 * @return array        the shuffled array
+	 * @param $in_array
+	 *
+	 * @return array
 	 */
 	private function shuffle_assoc($in_array) {
 		$keys = array_keys($in_array);
@@ -21,9 +28,16 @@ class PhileTwigShuffle extends \Phile\Plugin\AbstractPlugin implements \Phile\Ev
 		return array_merge(array_flip($keys), $in_array);
 	}
 
+	/**
+	 * event method
+	 * @param string $eventKey
+	 * @param null   $data
+	 *
+	 * @return mixed|void
+	 */
 	public function on($eventKey, $data = null) {
 		if ($eventKey == 'template_engine_registered') {
-			$shuffle = new Twig_SimpleFunction('shuffle', function ($array) {
+			$shuffle = new \Twig_SimpleFunction('shuffle', function ($array) {
 				if (is_array($array)) {
 					return $this->shuffle_assoc($array);
 				} else {
--- README.md
+++ README.md
@@ -3,11 +3,15 @@ phileTwigShuffle
 
 Shuffle an associative array with Twig
 
-### Installation
+### 1.1 Installation (composer)
+```
+php composer.phar require phile/twig-shuffle:*
+```
+### 1.2 Installation (Download)
 
 * Install [Phile](https://github.com/PhileCMS/Phile)
-* Clone this repo into `plugins/phileTwigShuffle`
-* add `$config['plugins']['phileTwigShuffle'] = array('active' => true);` to your `config.php`
+* Clone this repo into `plugins/phile/twigShuffle`
+* add `$config['plugins']['phile\\twigShuffle'] = array('active' => true);` to your `config.php`
 
 ### Usage
 
